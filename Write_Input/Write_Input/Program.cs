9c2b564dfafb1b38fcdca4b6ced35067fbcf4851
aula <martinconcetti97@gmail.com>
2022-05-19 10:48:24 -0300

    c# positions

---
+++ Write_Input/Write_Input/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Write_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines =
+        {
+            "x", "y", "z"
+        };
+
+
+            Console.WriteLine("Enter username:");
+            string x = Console.ReadLine();
+            Console.WriteLine("Username is: " + x);
+
+
+        }
+    }
+}
